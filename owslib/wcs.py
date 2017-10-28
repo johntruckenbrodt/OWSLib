80e8a11b3963f5f6ebd05ebc7e12438d90bd719a
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2017-10-28 16:46:11 +0200

    simplified initialization of cws objects

--- owslib/wcs.py
+++ owslib/wcs.py
@@ -21,7 +21,7 @@ from owslib.util import clean_ows_url, openURL
 
 
 def WebCoverageService(url, version=None, xml=None, cookies=None, timeout=30):
-    ''' wcs factory function, returns a version specific WebCoverageService object '''
+    """wcs factory function, returns a version specific WebCoverageService object"""
 
     if version is None:
         if xml is None:
@@ -36,8 +36,8 @@ def WebCoverageService(url, version=None, xml=None, cookies=None, timeout=30):
     clean_url = clean_ows_url(url)
 
     if version == '1.0.0':
-        return wcs100.WebCoverageService_1_0_0.__new__(wcs100.WebCoverageService_1_0_0, clean_url, xml, cookies)
+        return wcs100.WebCoverageService_1_0_0(clean_url, xml, cookies)
     elif version == '1.1.0':
-        return wcs110.WebCoverageService_1_1_0.__new__(wcs110.WebCoverageService_1_1_0, clean_url, xml, cookies)
+        return wcs110.WebCoverageService_1_1_0(clean_url, xml, cookies)
     elif version == '1.1.1':
-        return wcs111.WebCoverageService_1_1_1.__new__(wcs111.WebCoverageService_1_1_1, clean_url, xml, cookies)
+        return wcs111.WebCoverageService_1_1_1(clean_url, xml, cookies)
